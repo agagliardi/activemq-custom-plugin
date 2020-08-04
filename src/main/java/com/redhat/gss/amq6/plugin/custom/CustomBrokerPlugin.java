7c234326ee7913d9461831456c7a9ce9771c31ae
Antonio Gagliardi <antonio.gagliardi@gmail.com>
2020-08-04 16:53:57 +0200

    reproducer

--- src/main/java/com/redhat/gss/amq6/plugin/custom/CustomBrokerPlugin.java
+++ src/main/java/com/redhat/gss/amq6/plugin/custom/CustomBrokerPlugin.java
@@ -1,6 +1,9 @@
 package com.redhat.gss.amq6.plugin.custom;
 
+import java.nio.charset.StandardCharsets;
+
 import org.apache.activemq.broker.BrokerPluginSupport;
+import org.apache.activemq.command.ActiveMQMessage;
 import org.apache.activemq.command.ActiveMQTextMessage;
 import org.apache.activemq.command.Message;
 import org.apache.activemq.command.MessageDispatch;
@@ -11,15 +14,29 @@ public class CustomBrokerPlugin extends BrokerPluginSupport {
 
     private final static Logger log = LoggerFactory.getLogger(CustomBrokerPlugin.class);
 
+
     @Override
     public void postProcessDispatch(MessageDispatch messageDispatch) {
-        super.postProcessDispatch(messageDispatch);
-        final Message message = messageDispatch.getMessage();
-        if (message instanceof ActiveMQTextMessage) {
-            try {
-                log.info("Dispatched message with text {}", ((ActiveMQTextMessage) message).getText());
-            } catch (final Exception e) {
-                log.trace("Unable to get text from message {}", message, e);
+        if(messageDispatch != null) {
+            super.postProcessDispatch(messageDispatch);
+            final Message message = messageDispatch.getMessage();
+            if (message instanceof ActiveMQTextMessage) {
+                if (message != null && message.getContent() != null) {
+                    try {
+                        log.info("Messaggio POST PROCESS in TEXT scodato dalla coda: \"{}\", con testo: \"{}\"",
+                                ((ActiveMQTextMessage) message).getDestination().toString(),
+                                ((ActiveMQTextMessage) message).getText());
+                    } catch (final Exception e) {
+                        log.trace("Impossibile recuperare le informazioni sul testo del messaggio in arrivo in coda {}", message, e);
+                    }
+                }
+            } else if (message instanceof ActiveMQMessage) {
+                if (message != null && message.getContent() != null) {
+                    byte[] bytes = message.getContent().getData();
+                    String text = new String(bytes, StandardCharsets.UTF_8);
+                    log.info("Messaggio POST PROCESS in BYTE scodato dalla coda: \"{}\", con testo: \"{}\"",
+                            ((ActiveMQMessage) message).getDestination().toString(), text);
+                }
             }
         }
     }
